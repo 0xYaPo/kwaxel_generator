4018e4456ad2cb801994493fbf900e59b54cc734
YaPo <113506317+0xYaPo@users.noreply.github.com>
2025-08-19 12:37:31 +0000

    Dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+  },
+  resolve: {
+    alias: {
+      'react': 'react',
+      'react-dom': 'react-dom'
+    }
+  }
 })
